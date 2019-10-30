bcbc8e04c43cdd7403e87b0813c3600c0029254b
Venj <ersaclarke@gmail.com>
2019-10-30 13:04:44 +0800

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,13 +1,20 @@
-//
-//  Package.swift
-//  M13Checkbox
-//
-//  Created by McQuilkin, Brandon on 4/13/16.
-//  Copyright © 2016 Brandon McQuilkin. All rights reserved.
-//
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "M13Checkbox"
+    name: "M13Checkbox",
+    platforms: [.iOS(.v8)],
+    products: [
+        .library(
+            name: "M13Checkbox",
+            targets: ["M13Checkbox"]),
+    ],
+    targets: [
+        .target(
+            name: "M13Checkbox",
+            path: "Sources"),
+    ],
+    swiftLanguageVersions: [.v5]
 )
